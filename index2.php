df156b80d92017740f12a06fd5c361ad2cd58f65
afist <barahuda@ua.fm>
2018-05-08 17:56:37 +0300

    1.1.2

--- index2.php
+++ index2.php
@@ -29,12 +29,12 @@ require_once "autoload.php";
 $localhost ="localhost";
 $brigade = "brigade";
 try {
-    $connect = new PDO( "mysql:host=$localhost;dbname=$brigade1", 'root', '');
-    $sql = "CREATE DATABASE testDB2";
+    $connect = new PDO( "mysql:host=$localhost;dbname=$brigade", 'root', '');
+    $sql = "CREATE TABLE `testDB`.`brigadier` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , `description` VARCHAR(255) NOT NULL , `dailySalary` INT(30) NOT NULL , `position` VARCHAR(255) NOT NULL , `idWorker` VARCHAR(255) NOT NULL , `changeDate` TEXT NOT NULL , `daysWorked` INT(30) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
     $connect->exec($sql);
-    $sql="INSERT INTO worker (`Name`, `listOfTools`, `position`, `dailySalary`, `idBrigadier`, `changeDate`, `daysWorked`) 
-        VALUES( 'Valerian', 'work house', 333, 'brigadier', 'id5', '30')";
-        $connect->exec($sql);
+    // $sql="INSERT INTO worker (`Name`, `listOfTools`, `position`, `dailySalary`, `idBrigadier`, `changeDate`, `daysWorked`) 
+    //     VALUES( 'Valerian', 'work house', 333, 'brigadier', 'id5', '30')";
+    //     $connect->exec($sql);
 } catch (PDOException $e) {
     echo $sql.$e->getMessage();
 }
